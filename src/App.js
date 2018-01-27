a4569214ca742157bcd24d5dde675ba4affefc41
Juntao Qiu <juntao.qiu@gmail.com>
2018-01-27 12:51:17 +0800

    refactor the render

--- src/App.js
+++ src/App.js
@@ -13,12 +13,20 @@ class App extends Component {
       term: ''
     }
     this.filterBook = this.filterBook.bind(this)
+    this.renderBookDetail = this.renderBookDetail.bind(this)
   }
 
   filterBook(e) {
     this.setState({
       term: e.target.value
     })
+  }
+
+  renderBookDetail(event) {
+    const { books } = this.props
+    const result = books.filter((book) => book.id === parseInt(event.match.params.id, 10))
+
+    return <BookDetail book={result[0]} />
   }
 
   render() {
@@ -30,10 +38,7 @@ class App extends Component {
         <input type="text" onChange={this.filterBook} placeholder="Type to search..."/>
         <main>
           <Route exact path="/" render={() => <BookList books={filtered} />} />
-          <Route path="/books/:id" render={(event) => {
-            const books = filtered.filter((book) => book.id === parseInt(event.match.params.id, 10))
-            return <BookDetail book={books[0]} />
-          }} />
+          <Route path="/books/:id" render={(event) => this.renderBookDetail(event)} />
         </main>
       </div>
     );
--- src/App.spec.js
+++ src/App.spec.js
@@ -5,7 +5,7 @@ import {shallow, mount} from 'enzyme'
 
 import App from './App'
 
-import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter, Route } from 'react-router-dom'
 const mountWithRouter = (node) => mount(<MemoryRouter>{node}</MemoryRouter>)
 
 test('Search box', t => {
@@ -29,3 +29,10 @@ test('Search book based on title', t => {
   t.is(wrapper.find('.book').length, 1)
 })
 
+test('Routes', t => {
+  const wrapper = shallow(<App books={[]} />)
+
+  t.is(wrapper.find('Route').length, 2)
+  t.is(wrapper.find('Route').at(0).prop('path'), '/')
+  t.is(wrapper.find('Route').at(1).prop('path'), '/books/:id')
+})
